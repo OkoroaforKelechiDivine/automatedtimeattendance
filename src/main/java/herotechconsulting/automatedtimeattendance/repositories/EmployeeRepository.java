3825969cdb73176e2dfbcc6615ded785f43ebe9c
theRealNonso <okoroaforkelechi123@gmail.com>
2023-07-25 15:48:37 +0000

    fixed employee repository class

---
+++ src/main/java/herotechconsulting/automatedtimeattendance/repositories/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package herotechconsulting.automatedtimeattendance.repositories;
+
+import herotechconsulting.automatedtimeattendance.models.Employee;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends MongoRepository<Employee, String> {
+}
