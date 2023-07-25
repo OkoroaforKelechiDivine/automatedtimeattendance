c6b5d434570e91e56ea505feb3054d12e6036df0
theRealNonso <okoroaforkelechi123@gmail.com>
2023-07-25 15:54:57 +0000

    cleaned white spaces

--- src/main/java/herotechconsulting/automatedtimeattendance/repositories/AttendanceRepository.java
+++ src/main/java/herotechconsulting/automatedtimeattendance/repositories/AttendanceRepository.java
@@ -9,6 +9,5 @@ import java.util.List;
 
 @Repository
 public interface AttendanceRepository extends MongoRepository<Attendance, String> {
-
     List<Attendance> findByEmployee(Employee employee);
 }
