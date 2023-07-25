b2c474cf622d08147c6c5c68e325fb7ba50f54bb
theRealNonso <okoroaforkelechi123@gmail.com>
2023-07-25 19:19:05 +0000

    removed method

--- src/main/java/herotechconsulting/automatedtimeattendance/repositories/AttendanceRepository.java
+++ src/main/java/herotechconsulting/automatedtimeattendance/repositories/AttendanceRepository.java
@@ -1,7 +1,6 @@
 package herotechconsulting.automatedtimeattendance.repositories;
 
 import herotechconsulting.automatedtimeattendance.models.Attendance;
-import herotechconsulting.automatedtimeattendance.models.Employee;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +8,5 @@ import java.util.List;
 
 @Repository
 public interface AttendanceRepository extends MongoRepository<Attendance, String> {
-    List<Attendance> findByEmployee(Employee employee);
+
 }
