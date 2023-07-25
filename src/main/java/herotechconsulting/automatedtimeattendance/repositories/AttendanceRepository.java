820b8664ec4427868257dde83ed9998ad84125eb
theRealNonso <okoroaforkelechi123@gmail.com>
2023-07-25 19:19:26 +0000

    removed redundant comments

--- src/main/java/herotechconsulting/automatedtimeattendance/repositories/AttendanceRepository.java
+++ src/main/java/herotechconsulting/automatedtimeattendance/repositories/AttendanceRepository.java
@@ -4,7 +4,6 @@ import herotechconsulting.automatedtimeattendance.models.Attendance;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 
 @Repository
 public interface AttendanceRepository extends MongoRepository<Attendance, String> {
